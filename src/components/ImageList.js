aeff2d441a131b0139ff811716f30795093981db
Andrew <gskll@protonmail.com>
2020-11-25 14:24:45 +0100

    feat: Add tiling styles to image list
    Refactor image tag into ImageCard component
    Calculate the image height and set a correct number of spans

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ urls, id, alt_description }) => {
-    return <img src={urls.regular} key={id} alt={alt_description} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
